df4e39816f78b7c8278966422f125927352f2bfc
Ryan <rmh.sturrock@gmail.com>
2022-11-23 13:25:20 -0600

    created random index generators

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Random Wisdom Generator
 // Project that outputs random wisdom quotes
 
+const randomIndexSpiritualPractice = Math.floor(Math.random() * 6);
+const randomIndexSpiritualSubject = Math.floor(Math.random() * 6);
+const randomIndexSpiritualResult = Math.floor(Math.random() * 6);
 
 const spiritualPractice = ['Meditating', 'Reflecting', 'Performing Yoga', 'Praying', 'Contemplating', 'Searching'];
 
 const spiritualSubject = ['under a banyan tree', 'on the impermanence of life', 'on a sacred mountain', 'to the one above', 'on awareness and the Self', 'for the enlightened path'];
 
-const spiritualResult = ['will bring preace', 'bears the fruit of enlightenment', 'will cultivate a strong mind and body', 'will fill you with joy', 'grounds you in Being', 'will lead you back to the start of your journey'];
+const spiritualResult = ['will bring you peace', 'bears the fruit of enlightenment', 'will cultivate a strong mind and body', 'will fill you with joy', 'grounds you in Being', 'will lead you back to the start of your journey'];
+
+console.log(spiritualPractice[randomIndexSpiritualPractice]);
+console.log(spiritualSubject[randomIndexSpiritualSubject]);
+console.log(spiritualResult[randomIndexSpiritualResult]);
