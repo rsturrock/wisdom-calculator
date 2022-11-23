ce89e90dcc92d9441e838cad8bbc03342286bbe7
Ryan <rmh.sturrock@gmail.com>
2022-11-23 13:19:48 -0600

    created 3 arrays

---
+++ index.js
@@ -0,0 +1,9 @@
+// Random Wisdom Generator
+// Project that outputs random wisdom quotes
+
+
+const spiritualPractice = ['Meditating', 'Reflecting', 'Performing Yoga', 'Praying', 'Contemplating', 'Searching'];
+
+const spiritualSubject = ['under a banyan tree', 'on the impermanence of life', 'on a sacred mountain', 'to the one above', 'on awareness and the Self', 'for the enlightened path'];
+
+const spiritualResult = ['will bring preace', 'bears the fruit of enlightenment', 'will cultivate a strong mind and body', 'will fill you with joy', 'grounds you in Being', 'will lead you back to the start of your journey'];
