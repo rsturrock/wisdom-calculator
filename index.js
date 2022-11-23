422f7db9d840d104106fce8993f8456651920658
Ryan <rmh.sturrock@gmail.com>
2022-11-23 13:41:59 -0600

    removed randomWordsOfWisdom Array, returned string

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Random Wisdom Generator
 // Project that outputs random wisdom quotes
 
-const randomIndexSpiritualPractice = Math.floor(Math.random() * 6);
-const randomIndexSpiritualSubject = Math.floor(Math.random() * 6);
-const randomIndexSpiritualResult = Math.floor(Math.random() * 6);
+let randomIndexSpiritualPractice = Math.floor(Math.random() * 6);
+let randomIndexSpiritualSubject = Math.floor(Math.random() * 6);
+let randomIndexSpiritualResult = Math.floor(Math.random() * 6);
 
 const spiritualPractice = ['Meditating', 'Reflecting', 'Performing Yoga', 'Praying', 'Contemplating', 'Searching'];
 
@@ -11,12 +11,13 @@ const spiritualSubject = ['under a banyan tree', 'on the impermanence of life', 
 
 const spiritualResult = ['will bring you peace', 'bears the fruit of enlightenment', 'will cultivate a strong mind and body', 'will fill you with joy', 'grounds you in Being', 'will lead you back to the start of your journey'];
 
-let randomWordsOfWisdom = [];
-
 const randomizeWordsOfWisdom = () => {
-    randomWordsOfWisdom.push(`${spiritualPractice[randomIndexSpiritualPractice]} ${spiritualSubject[randomIndexSpiritualSubject]} ${spiritualResult[randomIndexSpiritualResult]}.`)
 
-    return randomWordsOfWisdom;
+    let randomIndexSpiritualPractice = Math.floor(Math.random() * 6);
+    let randomIndexSpiritualSubject = Math.floor(Math.random() * 6);
+    let randomIndexSpiritualResult = Math.floor(Math.random() * 6);
+
+    return `${spiritualPractice[randomIndexSpiritualPractice]} ${spiritualSubject[randomIndexSpiritualSubject]} ${spiritualResult[randomIndexSpiritualResult]}.`
 }
 
-console.log(randomizeWordsOfWisdom())
+console.log(randomizeWordsOfWisdom());
